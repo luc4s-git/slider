633419f69e5f604ce5fbc42d1226fdb21eba18a4
luc4s-git <lucassantos4332@hotmail.com>
2023-12-06 13:18:39 -0300

    import data and setup state value for personList and longPersonList

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,6 +2,8 @@ import { shortList, list, longList } from '../data';
 import { useState } from 'react';
 
 export default function Carousel() {
-  console.log(shortList, list, longList);
+  const [personList, setPersonList] = useState(list);
+  const [longPersonList, setLongPersonList] = useState(longList);
+
   return <h1>Carousel</h1>;
 }
