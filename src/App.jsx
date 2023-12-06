acd7e532ebd7b02be0e4313fc16c3f94fd25c06f
luc4s-git <lucassantos4332@hotmail.com>
2023-12-06 13:16:13 -0300

    Carousel component starter

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import Carousel from './components/Carousel';
+
 const App = () => {
   return (
     <main>
       <h2>Slider Starter</h2>
+      <Carousel></Carousel>
     </main>
   );
 };
+
 export default App;
---
+++ src/components/Carousel.jsx
@@ -0,0 +1,7 @@
+import { shortList, list, longList } from '../data';
+import { useState } from 'react';
+
+export default function Carousel() {
+  console.log(shortList, list, longList);
+  return <h1>Carousel</h1>;
+}
