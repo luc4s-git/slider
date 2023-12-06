683fc54e6383d1e1d77d30cccb4ae7d5de49fbdc
luc4s-git <lucassantos4332@hotmail.com>
2023-12-06 15:17:14 -0300

    slider css

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,29 +1,34 @@
 import { shortList, list, longList } from '../data';
 import { useState } from 'react';
-import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 export default function Carousel() {
+  const [currentPerson, setCurrentPerson] = useState(0);
   const [people, setPeople] = useState(list);
   const [longPeopleList, setLongPeopleList] = useState(longList);
 
+  const prevSlide = () => {};
+  const nextSlide = () => {};
+
   return (
-    <div className="slider-container ">
-      {shortList.map((person) => {
+    <section className="slider-container ">
+      {longPeopleList.map((person, index) => {
         return (
-          <div key={person.id} className="slide">
+          <article key={person.id} className="slide">
             <img src={person.image} alt={person.name} className="person-img" />
             <h5 className="name">{person.name}</h5>
             <p className="title">{person.title}</p>
             <p className="text">{person.quote}</p>
-          </div>
+            <FaQuoteRight className="icon" />
+          </article>
         );
       })}
-      <button className="prev">
-        <MdKeyboardArrowLeft />
+      <button className="prev" onClick={prevSlide}>
+        <FaChevronLeft />
       </button>
-      <button className="next">
-        <MdKeyboardArrowRight />
+      <button className="next" onClick={nextSlide}>
+        <FaChevronRight />
       </button>
-    </div>
+    </section>
   );
 }
